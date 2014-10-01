5aa46d26d61a34cd4e29620025edc709a223c187
BlueSurfRhino <richowens@outlook.com>
2014-10-01 15:18:59 -0700

    Successfully integrated NUnit tests

---
+++ Coding-Interviews/UnitTests/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Core;
+using NUnit.Framework;
+using Coding_Interviews_Lib;
+
+namespace UnitTests
+{
+    public class Class1
+    {
+        [TestFixture]
+        public class UnitTests
+        {
+            [Test]
+            public void MatrixTest()
+            {
+                int[,] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 } };
+                Assert.AreEqual(Answers.SumMatrix(matrix), 78);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Coding-Interviews
 =================
 -  refactored out the answers for the coding questions into it's own lib.
 -  added vs tuned .gitignore and removed *.suo and bin/obj files so we just save up source code
+-  to add nunit tests framework see this blog - How to Nunit with VS - http://erraticdev.blogspot.com/2012/01/running-or-debugging-nunit-tests-from.html
